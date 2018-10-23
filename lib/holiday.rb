a00009142cdd83ee87f22137aeaa41cca74861cd
Rafael Batista <rafaelb24@yahoo.com>
2018-10-23 16:10:16 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |key, value|
+    puts key
+    value.each {|key, value| puts value}
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
