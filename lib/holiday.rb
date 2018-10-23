c0c8d1d38d95373314457e39c603f66c7c4a41a2
Rafael Batista <rafaelb24@yahoo.com>
2018-10-23 16:27:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key, value|
    puts "#{key.capitalize}:"
    value.each do |key, value|
-     puts "  #{key.capitalize}: #{value.join(",")}"
+     puts "  #{key.capitalize}: #{value.join(" ")}"
    end
  end
 end
